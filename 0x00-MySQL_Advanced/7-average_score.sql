958d0c255917e18a5e9c2f013f9ff75408edaf32
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 02:57:57 +0200

    Update 7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_score FLOAT;
+    DECLARE total_projects INT;
+    DECLARE average_score FLOAT;
+
+    SELECT SUM(score), COUNT(*) INTO total_score, total_projects
+    FROM corrections
+    WHERE user_id = user_id;
+
+    IF total_projects > 0 THEN
+        SET average_score = total_score / total_projects;
+    ELSE
+        SET average_score = 0;
+    END IF;
+
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = user_id;
+END;
