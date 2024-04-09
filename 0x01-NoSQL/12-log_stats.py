5128e1a16b36b47182141a4c8b284f8887cf6e33
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 20:21:12 +0200

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Provide some stats about Nginx logs stored in MongoDB
+"""
+
+def log_stats(mongo_collection):
+    """
+    Provide some stats about Nginx logs stored in MongoDB
+    """
+    count = mongo_collection.count_documents({})
+    print(f"{count} logs")
+    
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = mongo_collection.count_documents({ "method": method })
+        print(f"    method {method}: {method_count}")
+    
+    status_check_count = mongo_collection.count_documents({ "method": "GET", "path": "/status" })
+    print(f"{status_check_count} status check")
