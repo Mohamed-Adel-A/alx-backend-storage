a0d87be54d5965827478e9c07b500c0647ee288e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 03:23:46 +0200

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,7 +2,7 @@
 
 -- Create function SafeDiv that divides the first by the second number or returns 0 if the second number is equal to 0
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
