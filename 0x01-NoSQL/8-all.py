3bfd52de82fb6fa7bca29c9087130eb7fac25a75
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 20:14:08 +0200

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,4 +5,13 @@ List all documents in a collection
 
 
 def list_all(mongo_collection):
-    return list(mongo_collection.find({}))
+    """
+    Lists all documents in a collection.
+
+    Args:
+        mongo_collection: pymongo collection object.
+
+    Returns:
+        List of documents in the collection.
+    """
+    return list(mongo_collection.find())
