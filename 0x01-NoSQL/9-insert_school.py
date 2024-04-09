543699300c2566e438edec826733e5c004345884
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 20:15:45 +0200

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Insert a new document in a collection based on kwargs
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection: pymongo collection object.
+        **kwargs: key-value pairs to be inserted as document fields.
+
+    Returns:
+        New _id of the inserted document.
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
+
