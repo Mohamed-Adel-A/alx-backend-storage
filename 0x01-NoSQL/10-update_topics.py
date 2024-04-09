b8eea1778b332e0053ec23fcbe8f7d5fc5b06a62
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 20:17:17 +0200

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Change all topics of a school document based on the name
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+
+    Args:
+        mongo_collection: pymongo collection object.
+        name: School name to update.
+        topics: List of topics to be set for the school.
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
