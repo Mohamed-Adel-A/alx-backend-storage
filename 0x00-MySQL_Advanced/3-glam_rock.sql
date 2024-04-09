773812d912981cbe0e845f0d1387786d8fa7ed56
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 04:50:07 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- List glam rock bands ranked by longevity
-
-SELECT band_name, (2022 - formed) AS lifespan
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, 
+       DATEDIFF('2022-01-01', STR_TO_DATE(SUBSTRING_INDEX(lifespan, '-', 1), '%Y')) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
