6a9fcc79d97ddca92755761287c694072f879792
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-09 20:20:06 +0200

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Return the list of schools having a specific topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+
+    Args:
+        mongo_collection: pymongo collection object.
+        topic: Topic searched.
+
+    Returns:
+        List of school documents with the specified topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
